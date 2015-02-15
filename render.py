7aa1f19e3ff8f1b16b1c6c641a9ec819bf4a06ea
John Gunderman <johngunderman@gmail.com>
2015-02-14 19:45:31 -0800

    center it

--- render.py
+++ render.py
@@ -9,10 +9,13 @@ iteration = [
 
 
 def draw_iteration(iteration, level):
+    x_center = len(iteration) / 2.0
+    y_center = len(iteration[0]) / 2.0
     for row in range(len(iteration)):
         for column in range(len(iteration[row])):
             if iteration[row][column] is 1:
-                bpy.ops.mesh.primitive_cube_add(location=[row * 2,column * 2,level])
+                bpy.ops.mesh.primitive_cube_add(
+                    location=[(row - x_center) * 2, (column - y_center) * 2, level])
 
 draw_iteration(iteration, 0)
 
