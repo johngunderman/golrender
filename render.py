8c15a0d3722fb56e360cd004febbe175bf83f20b
John Gunderman <johngunderman@gmail.com>
2015-02-14 21:02:53 -0800

    fix the bugs. should work now

--- render.py
+++ render.py
@@ -16,27 +16,27 @@ def draw_iteration(iteration, level):
         for column in range(len(iteration[row])):
             if iteration[row][column] is 1:
                 bpy.ops.mesh.primitive_cube_add(
-                    location=[(row - x_center) * 2, (column - y_center) * 2, level])
+                    location=[(row - x_center) * 2, (column - y_center) * 2, level * 2])
 
 def generate_next_iteration(current_iteration):
     new_iteration = copy.deepcopy(current_iteration)
     for row in range(len(current_iteration)):
         for column in range(len(current_iteration[row])):
             neighbor_count = get_neighbor_count(current_iteration, row, column)
-        # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-        if current_iteration[row][column] is 1 and neighbor_count < 2:
-            new_iteration[row][column] = 0
-            continue
-        # Any live cell with two or three live neighbours lives on to the next generation.
-        if current_iteration[row][column] is 1 and neighbor_count <= 3:
-            continue
-        # Any live cell with more than three live neighbours dies, as if by overcrowding.
-        if current_iteration[row][column] is 1 and neighbor_count > 3:
-            new_iteration[row][column] = 0
-            continue
-        if current_iteration[row][column] is 0 and neighbor_count is 3:
-            new_iteration[row][column] = 1
-        # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+            if current_iteration[row][column] is 1 and neighbor_count < 2:
+                new_iteration[row][column] = 0
+                continue
+            # Any live cell with two or three live neighbours lives on to the next generation.
+            if current_iteration[row][column] is 1 and neighbor_count <= 3:
+                continue
+            # Any live cell with more than three live neighbours dies, as if by overcrowding.
+            if current_iteration[row][column] is 1 and neighbor_count > 3:
+                new_iteration[row][column] = 0
+                continue
+            # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            if current_iteration[row][column] is 0 and neighbor_count is 3:
+                new_iteration[row][column] = 1
     return new_iteration
 
 def get_neighbor_count(iteration, row, column):
@@ -44,24 +44,26 @@ def get_neighbor_count(iteration, row, column):
     max_row = len(iteration) - 2
     max_col = len(iteration[0]) - 2
     # D: D:
-    if iteration[row][column - 1] is 1 and column  >= 1:
-        count++
-    if iteration[row][column + 1] is 1 and column <= max_col
-        count++
-    if iteration[row - 1][column] is 1 and row >= 1:
-        count++
-    if iteration[row + 1][column] is 1 and row <= max_row:
-        count++
-    if iteration[row - 1][column - 1] is 1 and row >= 1 and column >= 1:
-        count++
-    if iteration[row + 1][column - 1] is 1 and row <= max_row and column >= 1:
-        count++
-    if iteration[row - 1][column + 1] is 1 and row >= 1 and column <= max_col:
-        count++
-    if iteration[row + 1][column + 1] is 1 and row <= max_row and column <= max_col:
-        count++
+    if column  >= 1 and iteration[row][column - 1] is 1:
+        count += 1
+    if column <= max_col and iteration[row][column + 1] is 1:
+        count += 1
+    if row >= 1 and iteration[row - 1][column] is 1:
+        count += 1
+    if row <= max_row and iteration[row + 1][column] is 1:
+        count += 1
+    if row >= 1 and column >= 1 and iteration[row - 1][column - 1] is 1:
+        count += 1
+    if row <= max_row and column >= 1 and iteration[row + 1][column - 1] is 1:
+        count += 1
+    if row >= 1 and column <= max_col and iteration[row - 1][column + 1] is 1:
+        count += 1
+    if row <= max_row and column <= max_col and iteration[row + 1][column + 1] is 1:
+        count += 1
+    return count
 
 draw_iteration(iteration, 0)
-
+next_iteration = generate_next_iteration(iteration)
+draw_iteration(next_iteration, 1);
 
 # exec(compile(open('/Users/johngunderman/src/golrender/render.py').read(), '/Users/johngunderman/src/golrender/render.py', 'exec'))
